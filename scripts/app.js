95e7b871779c562a1a170a421dd52608876d6775
Ryan Scotton <rscotton@weareo3.com>
2017-02-08 14:27:00 -0500

    More of the training

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-angular.module('todoListApp', []); // second param = dependency list. Leave blank to create new module
+angular.module('todoListApp', []) // second param = dependency list. Leave blank to create new module
+
+.controller('mainCtrl', function($scope) {
+	$scope.learningNgChange = function() {
+		console.log("An input changed!");
+	};
+
+	$scope.todos = [
+		{"name": "clean the house"},
+		{"name": "water the dog"},
+		{"name": "feed the lawn"},
+		{"name": "pay dem bills"},
+		{"name": "run"},
+		{"name": "swim"}
+	]
+}); 
